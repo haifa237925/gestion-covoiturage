ccd18ae37a0ace4d3457b64c312f7668d7231dd8
haifa237925 <haifa.helaoui@etudiant-fst.utm.tn>
2024-12-13 22:11:10 +0100

    Initial commit - Setup Spring Boot project

---
+++ src/main/java/com/gestion/covoiturage/gestioncovoiturage/GestionCovoiturageApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.covoiturage.gestioncovoiturage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCovoiturageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCovoiturageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestion/covoiturage/gestioncovoiturage/GestionCovoiturageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.covoiturage.gestioncovoiturage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCovoiturageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
